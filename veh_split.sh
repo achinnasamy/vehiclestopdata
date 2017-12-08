3429235ae56de03d43f8637c786e8a68e3eff43d
Chinnasamy <AChinnasamy-consultant@Scholastic.com>
2017-12-08 11:58:08 +0530

    Create veh_split.sh

---
+++ veh_split.sh
@@ -0,0 +1,55 @@
+
+
+# Streaming Editor Linux Command for splitting the file.
+
+sed -n '1,100000p' veh_stop.csv >> vs1.csv
+sed -n '100000,200000p' veh_stop.csv >> vs1.csv
+sed -n '200000,300000p' veh_stop.csv >> vs2.csv
+sed -n '300000,400000p' veh_stop.csv >> vs3.csv
+sed -n '400000,500000p' veh_stop.csv >> vs4.csv
+sed -n '500000,600000p' veh_stop.csv >> vs5.csv
+sed -n '600000,700000p' veh_stop.csv >> vs6.csv
+sed -n '700000,800000p' veh_stop.csv >> vs7.csv
+sed -n '800000,900000p' veh_stop.csv >> vs8.csv
+sed -n '900000,1000000p' veh_stop.csv >> vs9.csv
+sed -n '1000000,1100000p' veh_stop.csv >> vs10.csv
+sed -n '1100000,1200000p' veh_stop.csv >> vs11.csv
+sed -n '1200000,1300000p' veh_stop.csv >> vs12.csv
+sed -n '1300000,1400000p' veh_stop.csv >> vs13.csv
+sed -n '1400000,1500000p' veh_stop.csv >> vs14.csv
+sed -n '1500000,1600000p' veh_stop.csv >> vs15.csv
+sed -n '1600000,1700000p' veh_stop.csv >> vs16.csv
+sed -n '1700000,1800000p' veh_stop.csv >> vs17.csv
+sed -n '1800000,1900000p' veh_stop.csv >> vs18.csv
+sed -n '1900000,2000000p' veh_stop.csv >> vs19.csv
+
+sed -n '2000000,2100000p' veh_stop.csv >> vs20.csv
+sed -n '2100000,2200000p' veh_stop.csv >> vs21.csv
+sed -n '2200000,2300000p' veh_stop.csv >> vs22.csv
+sed -n '2300000,2400000p' veh_stop.csv >> vs23.csv
+sed -n '2400000,2500000p' veh_stop.csv >> vs24.csv
+sed -n '2500000,2600000p' veh_stop.csv >> vs25.csv
+sed -n '2600000,2700000p' veh_stop.csv >> vs26.csv
+sed -n '2700000,2800000p' veh_stop.csv >> vs27.csv
+sed -n '2800000,2900000p' veh_stop.csv >> vs28.csv
+sed -n '2900000,3000000p' veh_stop.csv >> vs29.csv
+
+sed -n '3000000,3100000p' veh_stop.csv >> vs30.csv
+sed -n '3100000,3200000p' veh_stop.csv >> vs31.csv
+sed -n '3200000,3300000p' veh_stop.csv >> vs32.csv
+sed -n '3300000,3400000p' veh_stop.csv >> vs33.csv
+sed -n '3400000,3500000p' veh_stop.csv >> vs34.csv
+sed -n '3500000,3600000p' veh_stop.csv >> vs35.csv
+sed -n '3600000,3700000p' veh_stop.csv >> vs36.csv
+sed -n '3700000,3800000p' veh_stop.csv >> vs37.csv
+sed -n '3800000,3900000p' veh_stop.csv >> vs38.csv
+sed -n '3900000,4012342p' veh_stop.csv >> vs39.csv
+
+
+
+
+
+
+
+
+
